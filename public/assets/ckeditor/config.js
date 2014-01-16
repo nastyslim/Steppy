74285b5907a1365ee20ff924b3d1113f4899160a
nastyslim <eduard.y.popov@gmail.com>
2014-01-16 09:28:18 -0800

    try

---
+++ public/assets/ckeditor/config.js
@@ -0,0 +1,4 @@
+(function() {
+  CKEDITOR.config.toolbar = [['Image', 'HorizontalRule', 'Smiley', 'SpecialChar'], ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'], ['NumberedList', 'BulletedList', '-', 'Blockquote'], ['Link', 'Unlink', 'Anchor', 'LinkToNode', 'LinkToMenu'], ['Format', 'Font', 'FontSize'], ['TextColor', 'BGColor'], ['Maximize']];
+
+}).call(this);
