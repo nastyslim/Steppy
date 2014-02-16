fc7e6768c6b33f8c0d6837920057c8e9804c395c
Eduard <withoutedpols@gmail.com>
2014-02-15 20:50:01 -0500

    font pg?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
 //= require ckeditor-jquery
 //= require_tree .
 //= require bootstrap.min
+//= require jquery
+//= require jquery_ujs
 
 $('.ckeditor').ckeditor({
   // optional config
